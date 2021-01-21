fc7a7591badfe2a3acb6ed06ff49c0731453bf67
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2021-01-21 18:30:12 +0300

    new commit

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,4 @@
-export const apiUrl =
-  // process.env.NODE_ENV === "production"
-  //   ?
-  "https://traveltrackerserver.herokuapp.com";
-// : "http://localhost:4000";
+export const apiUrl = "https://traveltrackerserver.herokuapp.com";
 
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const geoUrl =
--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -19,10 +19,10 @@ const loginSuccess = (userWithToken) => {
   };
 };
 
-// const tokenStillValid = (userWithoutToken) => ({
-//   type: TOKEN_STILL_VALID,
-//   payload: userWithoutToken,
-// });
+const tokenStillValid = (userWithoutToken) => ({
+  type: TOKEN_STILL_VALID,
+  payload: userWithoutToken,
+});
 
 export const logOut = () => ({ type: LOG_OUT });
 
@@ -79,37 +79,37 @@ export const login = (email, password) => {
   };
 };
 
-// export const getUserWithStoredToken = () => {
-//   return async (dispatch, getState) => {
-//     // get token from the state
-//     const token = localStorage.getItem("jwt");
-//     console.log(`fgsjgbstjgstgjsg`, token);
-//     // if we have no token, stop
-//     if (token === null) return;
+export const getUserWithStoredToken = () => {
+  return async (dispatch, getState) => {
+    // get token from the state
+    const token = localStorage.getItem("jwt");
+    console.log(`fgsjgbstjgstgjsg`, token);
+    // if we have no token, stop
+    if (token === null) return;
 
-//     dispatch(appLoading());
-//     try {
-//       // if we do have a token,
-//       // check wether it is still valid or if it is expired
-//       const response = await axios.get(`${apiUrl}/me`, {
-//         headers: { Authorization: `Bearer ${token}` },
-//       });
+    dispatch(appLoading());
+    try {
+      // if we do have a token,
+      // check wether it is still valid or if it is expired
+      const response = await axios.get(`${apiUrl}/me`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
 
-//       dispatch(loginSuccess(response));
+      dispatch(loginSuccess(response));
 
-//       // token is still valid
-//       dispatch(tokenStillValid(response.data));
-//       dispatch(appDoneLoading());
-//     } catch (error) {
-//       if (error.response) {
-//         console.log(error.response.message);
-//       } else {
-//         console.log(error);
-//       }
-//       // if we get a 4xx or 5xx response,
-//       // get rid of the token by logging out
-//       dispatch(logOut());
-//       dispatch(appDoneLoading());
-//     }
-//   };
-// };
+      // token is still valid
+      dispatch(tokenStillValid(response.data));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.message);
+      } else {
+        console.log(error);
+      }
+      // if we get a 4xx or 5xx response,
+      // get rid of the token by logging out
+      dispatch(logOut());
+      dispatch(appDoneLoading());
+    }
+  };
+};
